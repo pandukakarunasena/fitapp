a02ab4c60bc26bb4f4e7b251e440e7517985d2c0
DhanukaNK <dhanukakarunasena@gmail.com>
2023-07-05 15:42:43 +0530

    Add HomePage as the main page of the app

---
+++ Views/HomePage.xaml.cs
@@ -0,0 +1,31 @@
+using FitApp.Repositories;
+using FitApp.Models;
+
+namespace FitApp.Views;
+
+public partial class HomePage : ContentPage
+{
+    int count = 0;
+
+    public HomePage()
+    {
+        InitializeComponent();
+        User user = UserRepository.GetUser();
+        lblName.Text = user.Name;
+    }
+
+    private void btnAddWorkout_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddWorkoutPage));
+    }
+
+    private void btnAddCheatMeal_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddCheatMeal));
+    }
+
+    private void btnToReport_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(ReportPage));
+    }
+}
