4275640c779a62aaac3fc14851a4ec6eac58ff48
DhanukaNK <dhanukakarunasena@gmail.com>
2023-07-05 15:43:24 +0530

    Add cheat meal selection implementation with UI

--- Views/AddCheatMeal.xaml.cs
+++ Views/AddCheatMeal.xaml.cs
@@ -1,9 +1,33 @@
+using FitApp.Repositories;
+using FitApp.Models;
+using System.Collections.ObjectModel;
+
 namespace FitApp.Views;
 
 public partial class AddCheatMeal : ContentPage
 {
-	public AddCheatMeal()
+
+    ObservableCollection<CheatMeal> CheatMeals;
+    public AddCheatMeal()
 	{
 		InitializeComponent();
-	}
+        CheatMeals = new ObservableCollection<CheatMeal>(CheatMealRepository.GetAllCheatMeals());
+        CheatMealListView.ItemsSource = CheatMeals;
+    }
+
+    private void OnCheatMealSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        // Deselect the item
+        ((ListView)sender).SelectedItem = null;
+    }
+    private void btnSelectCheatMeal_Clicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        var cheatMeal = button.BindingContext as CheatMeal;
+        if (cheatMeal != null)
+        {
+            // Perform actions for the selected workout
+            DisplayAlert("Clicked", $"Cheat Meal '{cheatMeal.Name}' clicked!", "CLOSE");
+        }
+    }
 }
