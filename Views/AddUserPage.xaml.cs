9fd451d6e946b780a77a95836a03cdc80e8dc009
DhanukaNK <dhanukakarunasena@gmail.com>
2023-07-05 15:44:04 +0530

    Implement add user page UI and functionality

--- Views/AddUserPage.xaml.cs
+++ Views/AddUserPage.xaml.cs
@@ -1,3 +1,6 @@
+using FitApp.Models;
+using FitApp.Repositories;
+
 namespace FitApp.Views;
 
 public partial class AddUserPage : ContentPage
@@ -6,4 +9,28 @@ public partial class AddUserPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnSave_Clicked(object sender, EventArgs e)
+    {
+        string name = nameEntry.Text;
+        double height = double.Parse(heightEntry.Text);
+        double weight = double.Parse(weightEntry.Text);
+        string activityLevel = activityLevelPicker.SelectedItem as string;
+
+        User userProfile = new User
+        {
+            Name = name,
+            Height = height,
+            Weight = weight,
+            //ActivityLevel = (activityLevel)Enum.Parse(typeof(ActivityLevel), activityLevel)
+        };
+
+        // Save the user profile or perform any other required actions
+        User owner = UserRepository.AddUser(userProfile);
+
+        // Display a success message
+        DisplayAlert("Success", "User profile saved successfully!", "OK");
+
+		Shell.Current.GoToAsync(nameof(HomePage));
+    }
 }
