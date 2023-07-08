9bd7b0c4c22d05f864594bf09784c00750cd8121
DhanukaNK <dhanukakarunasena@gmail.com>
2023-07-08 09:18:49 +0530

    Re disign the modals

--- Views/Modals/CheatMealDetailsModal.xaml.cs
+++ Views/Modals/CheatMealDetailsModal.xaml.cs
@@ -1,3 +1,5 @@
+using FitApp.Models;
+
 namespace FitApp.Views.Modals;
 
 public partial class CheatMealDetailsModal : ContentPage
@@ -6,4 +8,16 @@ public partial class CheatMealDetailsModal : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void OnSaveClicked(object sender, EventArgs e)
+    {
+        string consumedAmount = consumedEntry.Text;
+
+        // Pass the captured cheat meal details back to the previous page or process it as needed
+        MessagingCenter.Send<CheatMealDetailsModal, string>
+            (this, "CheatMealDetail", consumedAmount);
+
+        // Dismiss the modal page
+        Application.Current.MainPage.Navigation.PopModalAsync();
+    }
 }
--- Views/Modals/WorkoutDetailsModalPage.xaml.cs
+++ Views/Modals/WorkoutDetailsModalPage.xaml.cs
@@ -11,12 +11,13 @@ public partial class WorkoutDetailsModalPage : ContentPage
 
     private void OnSaveClicked(object sender, EventArgs e)
     {
-        string amountCompleted = amountEntry.Text;
-        string intensity = intensityPicker.SelectedItem as string;
+
+        float weight = float.Parse(weightEntry.Text);
+        string completedAmount = amountEntry.Text;
 
         // Pass the captured workout details back to the previous page or process it as needed
         MessagingCenter.Send<WorkoutDetailsModalPage, WorkoutDetail>
-            (this, "WorkoutDetail", new WorkoutDetail(amountCompleted,intensity));
+            (this, "WorkoutDetail", new WorkoutDetail(completedAmount, weight));
 
         // Dismiss the modal page
         Application.Current.MainPage.Navigation.PopModalAsync();
